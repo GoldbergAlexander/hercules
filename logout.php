a5c8b348637431c7fdc650146f372213af649300
Alexander Goldberg <ag76052@uga.edu>
2017-02-06 16:36:24 -0800

    clean up

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "dbconnect.php";
-if(isset($_SESSION)){
-	session_unset();
-	session_destroy();
-	echo "<div class='logout' id='logout'>";
-	echo "Succesfuly Logged Out";
-	echo "</div>";
-}
--- managment.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<div class='password'></div>";
-include 'usermanagment.php';
-include 'locationmanagment.php';
-include 'groupmanagment.php';
